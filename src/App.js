e294c4bdd165289e50c7d4054456533c72ca346b
rickyrattlesnake <ricky.ratnayake@gmail.com>
2018-04-25 13:13:33 +1000

    fix: relative paths

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/responsive-resume/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
